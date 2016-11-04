568ded3a78c89ff350b8c6c882a6da668e0934d0
David Vasquez <david.vasquez@sundevs.com>
2016-11-04 11:14:16 -0500

    add

--- gulpfile.js
+++ gulpfile.js
@@ -8,16 +8,16 @@ var rename = require("gulp-rename");
 var uglify = require('gulp-uglify');
 var pkg = require('./package.json');
 
-
+// Set the banner content
 var banner = ['/*!\n',
     ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-    ' * Copyright 2016-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+    ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
     ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n',
     ' */\n',
     ''
 ].join('');
 
-
+// Compile LESS files from /less into /css
 gulp.task('less', function() {
     return gulp.src('less/agency.less')
         .pipe(less())
@@ -28,7 +28,7 @@ gulp.task('less', function() {
         }))
 });
 
-
+// Minify compiled CSS
 gulp.task('minify-css', ['less'], function() {
     return gulp.src('css/agency.css')
         .pipe(cleanCSS({ compatibility: 'ie8' }))
@@ -39,7 +39,7 @@ gulp.task('minify-css', ['less'], function() {
         }))
 });
 
-
+// Minify JS
 gulp.task('minify-js', function() {
     return gulp.src('js/agency.js')
         .pipe(uglify())
@@ -51,7 +51,7 @@ gulp.task('minify-js', function() {
         }))
 });
 
-
+// Copy vendor libraries from /node_modules into /vendor
 gulp.task('copy', function() {
     gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
         .pipe(gulp.dest('vendor/bootstrap'))
@@ -60,19 +60,20 @@ gulp.task('copy', function() {
         .pipe(gulp.dest('vendor/jquery'))
 
     gulp.src([
-            'vendor/font-awesome/**',
-            '!vendor/font-awesome/**/*.map',
-            '!vendor/font-awesome/.npmignore',
-            '!vendor/font-awesome/*.txt',
-            '!vendor/font-awesome/*.md',
-            '!vendor/font-awesome/*.json'
+            'node_modules/font-awesome/**',
+            '!node_modules/font-awesome/**/*.map',
+            '!node_modules/font-awesome/.npmignore',
+            '!node_modules/font-awesome/*.txt',
+            '!node_modules/font-awesome/*.md',
+            '!node_modules/font-awesome/*.json'
         ])
         .pipe(gulp.dest('vendor/font-awesome'))
 })
 
-
+// Run everything
 gulp.task('default', ['less', 'minify-css', 'minify-js', 'copy']);
 
+// Configure the browserSync task
 gulp.task('browserSync', function() {
     browserSync.init({
         server: {
@@ -81,17 +82,18 @@ gulp.task('browserSync', function() {
     })
 })
 
-
+// Dev task with browserSync
 gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function() {
     gulp.watch('less/*.less', ['less']);
     gulp.watch('css/*.css', ['minify-css']);
     gulp.watch('js/*.js', ['minify-js']);
-
+    // Reloads the browser whenever HTML or JS files change
     gulp.watch('*.html', browserSync.reload);
     gulp.watch('js/**/*.js', browserSync.reload);
 });
 
-
+// Compiles SCSS files from /scss into /css
+// NOTE: This theme uses LESS by default. To swtich to SCSS you will need to update this gulpfile by changing the 'less' tasks to run 'sass'!
 gulp.task('sass', function() {
     return gulp.src('scss/agency.scss')
         .pipe(sass())
