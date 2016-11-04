94591d6d59d04e5f6dbfe32864321c8509ac8f80
David Vasquez <david.vasquez@sundevs.com>
2016-11-04 11:02:20 -0500

    add

---
+++ cluster viejo/sectores.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Cluster Web - La Dorada</title>
+ <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+ <link type="text/css" rel="stylesheet" href="css/materialize.css" media="screen,projection" />
+     
+     <link type="text/css" rel="stylesheet" href="css/estilos.css" />
+    <link type="text/css" rel="stylesheet" href="css/custom.css" />
+    </head>
+    <body>
+    
+    <header class="orange lighten-2 z-depth-1">
+        <nav class="top-nav orange lighten-2 hdr">
+        <div class="nav-wrapper valign-wrapper" id="menu">
+        <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
+            <ul class="side-nav fixed #e53935 red darken-1" id="mobile-demo" style="left:-10px;">
+                        <li class=""><a href="" class="brand-logo " id="logo"><img src="img/logo.png" class="responsive-img"></a></li>
+                        <br>
+                    <li class="fuente iniciop"><a href="index.php#inicio" class="waves-effect waves-teal valign">Inicio</a>
+                    </li>
+                    <li class="fuente"><a href="index.php#sobremi" class="waves-effect valign">Sobre La Dorada</a>
+                    </li>
+                    <li class="sectores fuente"><a href="#sectores" class="waves-effect valign">Sectores</a>
+                    <ul>
+                    <li class="fuente2"><a href="" class="waves-effect valign">S. Primario</a></li>
+                    <li class="fuente2"><a href="" class="waves-effect valign">S. Secundario</a></li>
+                    <li class="fuente2"><a href="" class="waves-effect valign">S. Terciario</a></li>
+                    </ul>  
+                    </li>
+                    <li class="contacto fuente"><a href="index.php#contacto" class="waves-effect valign">Contacto</a>
+                    </li>
+                </ul>
+            </div>
+            </nav>
+        <div class=" valign-wrapper"><a class="valign cw">CLUSTER WEB</a></div>
+        <div class="valign-wrapper"><a class="valign ld">La Dorada,Caldas.</a></div>
+        <div class="valign-wrapper lg"><a class="valign lg" href="pag/ingresar.php"><img src="img/bd.png" href="pag/ingresar.php"></a></div>
+        </header>
+        <main>
+         <section id="inicio" class="inicio teal lighten-3">
+                     
+            <div class="container">
+                <h4 class="teal-text" ><b>Sectores</b></h4>
+                <hr class="teal-text">
+                <div class="row">
+                   <div class="col s12 center"><h5>Sector Primario</h5>
+                    </div>
+                      
+                    <div class="col s12 m12 l12">
+                  <p class="center" style="text-align: justify">El sector primario está formado por las actividades económicas relacionadas con la transformación de los recursos naturales en productos primarios no elaborados. Las principales actividades del sector primario son la agricultura, la ganadería, la silvicultura, la apicultura, la acuicultura, la caza, la pesca, explotación forestal y la minería. Usualmente, los productos primarios son utilizados como materia prima en las producciones industriales.</p>
+                       <div class="carousel">
+    <a class="carousel-item " href="#one!"><img src="img/sp/1.jpg"></a>
+    <a class="carousel-item" href="#two!"><img src="img/sp/2.jpg"></a>
+    <a class="carousel-item" href="#three!"><img src="img/sp/3.jpg"></a>
+    <a class="carousel-item" href="#four!"><img src="img/sp/4.jpg"></a>
+    
+  </div>
+                    </div>
+                    <div class="col s12 center"><h5>Sector Secundario</h5>
+                    </div>
+                    <div class="col s12 m12 l12">
+                  <p class="center" style="text-align: justify">El sector secundario comprende la artesanía, la industria, la construcción, la minería así como la obtención de energía.El sector secundario de la economía está compuesto por la industria manufacturera. Esta industria toma los materiales crudos y los convierte en productos. Por ejemplo, el acero se puede usar para fabricar autos. Los carpinteros toman la madera y hacen casas, muebles o gabinetes.</p>
+                         <div class="carousel">
+    <a class="carousel-item " href="#one!"><img src="img/ss/1.jpg"></a>
+    <a class="carousel-item" href="#two!"><img src="img/ss/2.jpg"></a>
+    <a class="carousel-item" href="#three!"><img src="img/ss/3.jpg"></a>
+    <a class="carousel-item" href="#four!"><img src="img/ss/4.jpg"></a>
+  </div>
+                    </div>
+                    <div class="col s12 center"><h5>Sector Terciario</h5>
+                    </div>
+                    <div class="col s12 m12 l12">
+                  <p class="center" style="text-align: justify">A nivel comercial, La Dorada se destaca por distribuir mercancías a precios módicos y de muy buena calidad. Actualmente, cuenta con un mall comercial (Santana del Río), inaugurado en 2015, donde cuenta con servicios de hospedaje, plazoleta de comidas, cine y locales comerciales. Se ofrece además servicios turísticos que realizan planes a los lugares más emblemáticos de La Dorada y sus alrededores. Se planea que el municipio cuente con su propio Centro Comercial, conocido como "Verano Mall". La Plaza Comercial "Dorada Plaza" fue inaugurado el 9 de julio de 2005, sobre el sector comercial principal. Esta construcción, indiscutiblemente ha elevado la economía de la mancomunidad a un nivel muy alto, lo cual, seguramente hace mucho más atractiva la ciudad para la gran cantidad de turistas que nos visitan.</p>
+                         <div class="carousel">
+     <a class="carousel-item " href="#one!"><img src="img/st/1.jpg"></a>
+    <a class="carousel-item" href="#two!"><img src="img/st/2.jpg"></a>
+    <a class="carousel-item" href="#three!"><img src="img/st/3.jpg"></a>
+    <a class="carousel-item" href="#four!"><img src="img/st/4.jpg"></a>
+  </div>
+                    </div>
+                </div>
+            </div>
+        </section>  
+        </main>
+            <footer class="page-footer orange lighten-2">
+            <div>
+                <div class="container center">
+                Analisis y Desarrollo de Sistemas de Informacion-Sena<br>
+                    <a class="grey-text text-lighten-4 " href="">© 2016 Davash07</a>
+                   </div>
+           
+             </div>
+           
+    </footer>
+        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+   <!-- <script type="text/javascript" src="js/jquery-2.2.3.min.js"></script>-->
+ <script type="text/javascript" src="js/materialize.min.js"></script>
+ <script type="text/javascript" src="js/js.js"></script>
+
+    </body>
+</html>
